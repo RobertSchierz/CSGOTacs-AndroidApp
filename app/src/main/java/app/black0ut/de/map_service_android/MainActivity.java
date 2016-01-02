1ccf8d610815f17ab337353567b74799e2735aff
blk0ut <altenhof.jp@gmx.de>
2016-01-02 23:45:14 +0100

    Vorschaubilder in den passenden Auflösungen für alle DPI-Stufen erzeugt und hinzugefügt. Kleine Änderungen am Layout vorgenommen.

--- app/src/main/java/app/black0ut/de/map_service_android/MainActivity.java
+++ app/src/main/java/app/black0ut/de/map_service_android/MainActivity.java
@@ -66,6 +66,7 @@ public class MainActivity extends AppCompatActivity
         //Methode zum Nachrichten senden aufrufen
         attemptSend();
 
+        /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,7 +75,7 @@ public class MainActivity extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
-
+        */
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
--- app/src/main/java/app/black0ut/de/map_service_android/adapter/MapsListViewAdapter.java
+++ app/src/main/java/app/black0ut/de/map_service_android/adapter/MapsListViewAdapter.java
@@ -15,6 +15,8 @@ import org.androidannotations.annotations.RootContext;
 import org.androidannotations.annotations.ViewById;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import app.black0ut.de.map_service_android.MapListItemView;
@@ -37,35 +39,32 @@ public class MapsListViewAdapter extends BaseAdapter{
     @AfterInject
     void initAdapter() {
         maps = new ArrayList<>();
-        maps.add(new Map("Dust 2", R.drawable.ic_map_orange_600_18dp));
-        maps.add(new Map("Train", R.drawable.ic_group_orange_600_18dp));
-        maps.add(new Map("Mirage", R.drawable.ic_group_orange_600_18dp));
-        maps.add(new Map("Inferno", R.drawable.ic_group_orange_600_18dp));
-        maps.add(new Map("Cobblestone", R.drawable.ic_group_orange_600_18dp));
-        maps.add(new Map("Overpass", R.drawable.ic_group_orange_600_18dp));
-        maps.add(new Map("Cache", R.drawable.ic_group_orange_600_18dp));
-        maps.add(new Map("Aztec", R.drawable.ic_group_orange_600_18dp));
-        maps.add(new Map("Dust", R.drawable.ic_group_orange_600_18dp));
-        maps.add(new Map("Vertigo", R.drawable.ic_group_orange_600_18dp));
-        maps.add(new Map("Nuke", R.drawable.ic_group_orange_600_18dp));
-        maps.add(new Map("Office", R.drawable.ic_group_orange_600_18dp));
-        maps.add(new Map("Italy", R.drawable.ic_group_orange_600_18dp));
-        maps.add(new Map("Assault", R.drawable.ic_group_orange_600_18dp));
-        maps.add(new Map("Militia", R.drawable.ic_group_orange_600_18dp));
-    }
+        maps.add(new Map("Dust 2", R.drawable.de_dust2));
+        maps.add(new Map("Train", R.drawable.de_train));
+        maps.add(new Map("Mirage", R.drawable.de_mirage));
+        maps.add(new Map("Inferno", R.drawable.de_inferno));
+        maps.add(new Map("Cobblestone", R.drawable.gd_cbble));
+        maps.add(new Map("Overpass", R.drawable.de_overpass));
+        maps.add(new Map("Cache", R.drawable.de_cache));
+        maps.add(new Map("Aztec", R.drawable.de_aztec));
+        maps.add(new Map("Dust", R.drawable.de_dust));
+        maps.add(new Map("Vertigo", R.drawable.de_vertigo));
+        maps.add(new Map("Nuke", R.drawable.de_nuke));
+        maps.add(new Map("Office", R.drawable.cs_office));
+        maps.add(new Map("Italy", R.drawable.cs_italy));
+        maps.add(new Map("Assault", R.drawable.cs_assault));
+        maps.add(new Map("Militia", R.drawable.cs_militia));
 
+        //Die Liste alphabetisch sortieren
+        Collections.sort(maps, new Comparator<Map>() {
+            public int compare(Map m1, Map m2) {
+                return m1.mapName.compareTo(m2.mapName);
+            }
+        });
+    }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        /*
-        LayoutInflater inflater = (LayoutInflater) context
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View rowView = inflater.inflate(R.layout.listview_icon_text, parent, false);
-        ImageView mapPreview = (ImageView) rowView.findViewById(R.id.map_preview);
-        TextView mapName = (TextView) rowView.findViewById(R.id.map_name);
-        mapPreview.setImageResource(R.drawable.ic_account_circle_orange_600_18dp);
-        mapName.setText(values[position]);
-        */
 
         MapListItemView mapListItemView;
         if(convertView == null){
