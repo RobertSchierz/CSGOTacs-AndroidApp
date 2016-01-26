1518edb695e8eea3fb8afd13cdd8493c2389f33f
blk0ut <altenhof.jp@gmx.de>
2016-01-26 23:20:30 +0100

    Es wurde ein neuer Button zum abmelden des Nutzers hinzugefügt.

--- app/src/main/java/app/black0ut/de/map_service_android/fragments/MyProfileDetailsFragment.java
+++ app/src/main/java/app/black0ut/de/map_service_android/fragments/MyProfileDetailsFragment.java
@@ -6,6 +6,8 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
@@ -17,6 +19,7 @@ import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.ViewById;
+import org.w3c.dom.Text;
 
 import app.black0ut.de.map_service_android.R;
 import app.black0ut.de.map_service_android.data.User;
@@ -45,6 +48,8 @@ public class MyProfileDetailsFragment extends android.support.v4.app.Fragment {
     EditText editTextNameSignedIn;
     @ViewById
     EditText editTextPasswordSignedIn;
+    @ViewById
+    TextView logoutButton;
 
     @AfterViews
     public void afterViews() {
@@ -81,6 +86,24 @@ public class MyProfileDetailsFragment extends android.support.v4.app.Fragment {
     }
 
     @Click
+    public void logoutButtonClicked(){
+        //Den User aus den SharedPrefs löschen damit er sich neu anmelden muss
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.remove(User.USERNAME);
+        editor.putBoolean(User.IS_LOGGED_IN, false);
+        editor.apply();
+        //Zurück zum Login Fragment
+        Fragment fragment = new MyProfileFragment_();
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .setCustomAnimations(R.anim.slide_out_left, R.anim.slide_in_right)
+                .replace(R.id.mainFrame, fragment)
+                .commit();
+        fragmentManager.executePendingTransactions();
+        Toast.makeText(getContext(), "Du hast Dich erfolgreich ausgeloggt.", Toast.LENGTH_SHORT).show();
+    }
+
+    @Click
     public void fabEditClicked(){
         //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
         //        .setAction("Action", null).show();
@@ -89,10 +112,11 @@ public class MyProfileDetailsFragment extends android.support.v4.app.Fragment {
         editTextPasswordSignedIn.setText(MyProfileFragment.password);
         avatarImage.setText("+");
         username.setVisibility(View.GONE);
+        logoutButton.setVisibility(View.GONE);
         //Animation Quelle: http://stackoverflow.com/questions/22454839/android-adding-simple-animations-while-setvisibilityview-gone
         editTextRelativeLayout.animate()
                 .alpha(1.0f)
-                .translationY(-username.getHeight())
+                //.translationY(-username.getHeight()-logoutButton.getHeight())
                 .setDuration(500)
                 .setListener(new AnimatorListenerAdapter() {
                     @Override
@@ -111,6 +135,7 @@ public class MyProfileDetailsFragment extends android.support.v4.app.Fragment {
         avatarImage.setText("Avatar");
         fabEdit.setVisibility(View.VISIBLE);
         username.setVisibility(View.VISIBLE);
+        logoutButton.setVisibility(View.VISIBLE);
 
     }
 }
